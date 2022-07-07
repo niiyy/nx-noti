baaf06a731aac85cd5579af9a353fbab67af8871
niiyy <smailaberkaoui@gmail.com>
2022-07-07 21:25:44 +0200

    v1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version "cerulean"
+
+description "FiveM noti."
+author "niiyy & Af0xz"
+version '1.0.0'
+
+games {
+  "gta5",
+}
+
+
+ui_page 'ui/public/index.html'
+
+files {
+  'ui/public/index.html',
+  'ui/public/**/*'
+}
--- ui/ts/debug/injectMock.ts
+++ ui/ts/debug/injectMock.ts
@@ -1,5 +1,3 @@
 class Mock {
-    constructor() {}
-
+  constructor() {}
 }
-
